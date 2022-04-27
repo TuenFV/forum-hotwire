68f3f353bc1e7520fa6411ca8764da47318da8ba
LoKi <sand5088@gmail.com>
2022-04-27 17:45:24 +0700

    Add discussion model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
+
+  has_many :discussions, dependent: :destroy
 end
