80739e0cb833a27f4ec78cc061a4c413e97aa182
LoKi <sand5088@gmail.com>
2022-04-27 23:48:40 +0700

    Discussion show with inline edit

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,6 +1,6 @@
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_discussion, only: [:edit, :update, :destroy]
+  before_action :set_discussion, only: [:show, :edit, :update, :destroy]
 
   def index
     @discussions = Discussion.all
@@ -15,7 +15,7 @@ class DiscussionsController < ApplicationController
 
     respond_to do |format|
       if @discussion.save
-        format.html { redirect_to discussions_path, notice: "Discussion created!" }
+        format.html { redirect_to @discussion, notice: "Discussion created!" }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -28,7 +28,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        format.html { redirect_to discussions_path, notice: "Discussion updated!" }
+        format.html { redirect_to @discussion, notice: "Discussion updated!" }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -38,6 +38,10 @@ class DiscussionsController < ApplicationController
   def destroy
     @discussion.destroy!
     redirect_to discussions_path, notice: "Discussion removed!"
+  end
+
+  def show
+
   end
 
   private
